9e9f734d87272ec031065270f7ef3e15903daed5
ziklagx <ziklagx@gmail.com>
2010-12-13 13:15:22 -0800

    sign in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,10 @@ class SessionsController < ApplicationController
 	
   end
   
+  def destroy
+	sign_out
+	redirect_to root_path
+  end
   
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ SampleApp::Application.routes.draw do
   match '/signin', :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
   
-  
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
