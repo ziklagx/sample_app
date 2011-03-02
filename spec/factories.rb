10ee162d44487de8fa33554580a2eb0ebf7171e0
ziklagx <ziklagx@gmail.com>
2011-03-02 09:15:12 -0800

    followed/followers pages

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,9 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
-	user.name					"New User"
-	user.email					"auser@example.com"
-	user.password				"foobar"
-	user.password_confirmation	"foobar"
+  user.name "Some User"
+  user.email "exampleuser@example.com"
+  user.password "foobar"
+  user.password_confirmation "foobar"
 end
 
 Factory.sequence :email do |n|
